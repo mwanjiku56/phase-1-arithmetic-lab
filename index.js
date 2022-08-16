1d3fcb211406f06454a6a46dc7dfc7fcc44c40d0
mercy wanjiku maruga <mercy.maruga@student.moringaschool.com>
2022-08-16 15:52:20 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 =2;
+let multiply = num1*num2;
 
-// Write your code here
+let random =Math.floor(Math.random()) +1; 
+/*
+Create a variable called mod set to an equation that 
+will calculate the remainder of dividing variable num3 by num4; 
+the remainder should be 4.
+
+*/
+let num3 = 34,num4 =5;
+let mod = num3%num4;
+
+
+let max = Math.max(20)
+
